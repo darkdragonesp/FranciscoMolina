3857a3575bf5d41ace4def683fb05e9cf2d104f5
usuario <usuario@192.168.183.136>
2018-11-12 19:20:06 +0100

    Cambio menor

--- Proyecto/src/es/arelance/proyecto/modelo/dao/UsuarioDao.java
+++ Proyecto/src/es/arelance/proyecto/modelo/dao/UsuarioDao.java
@@ -7,17 +7,6 @@ import es.arelance.proyecto.modelos.Usuario;
  *
  */
 public interface UsuarioDao {
-	/**
-	 * 
-	 * @param nombreUsuario
-	 * @param contrasena
-	 * @return
-	 */
 	Integer findByNameAndPass(String nombreUsuario,String contrasena);
-	/**
-	 * 
-	 * @param idUsuario
-	 * @return
-	 */
 	Usuario findById(Integer idUsuario);
 }
