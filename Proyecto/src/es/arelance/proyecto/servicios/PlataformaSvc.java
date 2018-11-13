e3ebcbbc23f016e9e92600565325462ff4bd80b0
Francisco Molina Sánchez <franmolina@users.noreply.github.com>
2018-11-13 19:24:49 +0100

    Cambio nombre Plataforma a PlataformaSvc
    Cambio nombre Plataforma a PlataformaSvc

--- Proyecto/src/es/arelance/proyecto/servicios/PlataformaSvc.java
+++ Proyecto/src/es/arelance/proyecto/servicios/PlataformaSvc.java
@@ -2,13 +2,15 @@ package es.arelance.proyecto.servicios;
 
 import java.util.List;
 
+import es.arelance.proyecto.modelo.Plataforma;
+
 /**
  * Servicio para plataforma dao
  * 
  * @author Francisco Molina Sanchez
  *
  */
-public interface Plataforma {
+public interface PlataformaSvc {
 	/**
 	 * Obtiene todas las plataformas disponibles
 	 * 
